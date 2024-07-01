3dc9b3be970d3f6731e33671cae81fdd0cc3b823
Luiz Fernando <luizfs138@gmail.com>
2024-07-01 11:21:05 -0300

    Atualizando login

--- frontend/public/login.js
+++ frontend/public/login.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const password = document.getElementById('password').value;
   
       try {
-        const response = await fetch('http://localhost:3000/api/login', {
+        const response = await fetch('https://sistema-de-atendimento-pub-sub.onrender.com/api/login', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
